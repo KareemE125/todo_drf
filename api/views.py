b71a024f5228a70410b6180d7f05ff2bd2384a0b
Kareem Ezzat <Kareemezzat.125@gmail.com>
2023-12-17 13:04:59 +0200

    Create Todo model and Todos endpont

--- api/views.py
+++ api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .models import Todo
+from .serializers import TodoSerializer
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    routes = [
+        'GET /api',
+        'GET /api/todos',
+        'GET /api/todo/<str:pk>',
+        'POST /api/todo-create',
+        'POST /api/todo-update/<str:pk>',
+        'DELETE /api/todo-delete/<str:pk>',
+    ]
+    
+    data = {
+        "description": "Todo API Overview",
+        "routes": routes
+    }
+    
+    return Response(data)
+
+@api_view(['GET'])
+def getAllTodos(request):
+    todos = TodoSerializer(Todo.objects.all(), many = True)
+    data ={
+        "description": "Get All Todos",
+        "todos": todos.data
+    }
+    
+    return Response(data)
+
+
+
+
+
+
+
